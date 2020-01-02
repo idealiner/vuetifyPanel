58a5a6e21b684cb3767b360081433341c8645606
idealiner <apps@idealiner.com>
2020-01-01 21:48:29 -0500

    Added foo to the bar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <v-toolbar fat app>
+            <v-btn flat color="grey"></v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            drawer: true
+        }
+    }
+}
+</script>>
